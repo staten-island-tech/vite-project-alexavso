60fb2f3d1b27b6f84d16f55e7b87748b157eed1c
alexavso <alexasolanou@gmail.com>
2025-12-04 12:32:24 -0500

    .

---
+++ vite-project/src/index.js
@@ -0,0 +1,48 @@
+import './style.css';
+
+const artwork = [
+    {
+        title: "The Japanese Footbridge",
+        artist: "Claude Monet",
+        created: 1889,
+    },
+
+    {
+        title: "Water Lily Pond",
+        artist: "Claude Monet",
+        created: 1900,
+    },
+
+    {
+        title: "Water Lily Pond 9",
+        artist: "Claude Monet",
+        created: 1919,
+    },
+
+    {
+        title: "Bouquet of Sunflowers",
+        artist: "Claude Monet",
+        created: 1881,
+    },
+
+    {
+        title: "Woman With a Parasol",
+        artist: "Claude Monet",
+        created: 1875,
+    },
+
+    {
+        title: "Waterloo Bridge",
+        artist: "Claude Monet",
+        created: 1903,
+    },
+
+    {
+        title: "Water Lilies at Sunset",
+        artist: "Claude Monet",
+        created: 1914,
+    },
+
+
+
+]
